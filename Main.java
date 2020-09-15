602683f503010f3b0b8319f6b3a82d605534a0c2
younj1601 <younj1601@wrdsb.ca>
2020-09-15 16:51:50 +0000

    Finished question 5 for conditions

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will be able to play fizzbuzz
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //prompt the user to enter integer
+    System.out.println("Please enter a number to play FizzBuzz");
+    //get the iteger from the user
+    int num1 = input.nextInt();
+
+    if ((num1 % 5 == 0) && (num1 % 3 == 0)){
+      System.out.println("You should say FizzBuzz");
+    } else if ((num1 % 3 == 0)){
+      System.out.println("You should say Fizz");
+    } else if ((num1 % 5 == 0)){
+      System.out.println("You should say Buzz");
+    } else if ((num1 % 3 != 0) && (num1 % 5 != 0)){
+      System.out.println("You should say " + num1);
+    }
+
   }
 }
